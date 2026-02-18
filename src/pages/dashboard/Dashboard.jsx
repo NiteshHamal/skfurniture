5ad79b2eff484510e0644add576798cfb4f55647
NiteshHamal <nitesh0hamal@gmail.com>
2026-02-18 15:05:47 +0545

    monthly activity chart dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Layout from '../../layout/Layout'
 import { Box, Grid, Typography } from '@mui/material'
 import Metricbar from '../../components/dashboard/Metricbar'
 import { Category, ContactMail, Inventory, People, Star, ToggleOn } from '@mui/icons-material'
 import { useRoutes } from 'react-router-dom'
+import MonthlyAcitvityChart from '../../components/dashboard/MonthlyAcitvityChart'
 
 const Dashboard = () => (
     <>
@@ -19,21 +19,21 @@ const Dashboard = () => (
 
             <Grid container spacing={2.5} sx={{ mb: 4 }}>
                 <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-                    <Metricbar 
-                    icon={<People/>}
-                    value={1}
-                    title="Total Users"
-                    iconBgColor='#ede9fe'
-                    iconColor='#7c3aed'
+                    <Metricbar
+                        icon={<People />}
+                        value={1}
+                        title="Total Users"
+                        iconBgColor='#ede9fe'
+                        iconColor='#7c3aed'
                     />
                 </Grid>
                 <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-                    <Metricbar 
-                    icon={<Category/>}
-                    value={27}
-                    title="Categories"
-                    iconBgColor='#fce7f3'
-                    iconColor='#ec4899'
+                    <Metricbar
+                        icon={<Category />}
+                        value={27}
+                        title="Categories"
+                        iconBgColor='#fce7f3'
+                        iconColor='#ec4899'
                     />
                 </Grid>
                 <Grid size={{ xs: 12, sm: 6, md: 4 }}>
@@ -47,7 +47,7 @@ const Dashboard = () => (
                 </Grid>
                 <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                     <Metricbar
-                        icon={<ToggleOn/>}
+                        icon={<ToggleOn />}
                         title={'Active Products'}
                         value={2}
                         iconBgColor={'#d1fae5'}
@@ -56,7 +56,7 @@ const Dashboard = () => (
                 </Grid>
                 <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                     <Metricbar
-                        icon={<Star/>}
+                        icon={<Star />}
                         title={'Featured'}
                         value={2}
                         iconBgColor={'#fef3c7'}
@@ -65,7 +65,7 @@ const Dashboard = () => (
                 </Grid>
                 <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                     <Metricbar
-                        icon={<ContactMail/>}
+                        icon={<ContactMail />}
                         title={'Contact'}
                         value={0}
                         iconBgColor={'#fee2e2'}
@@ -74,8 +74,32 @@ const Dashboard = () => (
                 </Grid>
             </Grid>
 
+            <Grid container spacing={2.5} sx={{ mb: 4 }}>
+                <Grid size={{ xs: 12 }} 
+                sx={{ 
+                    border: '1px solid #e2e8f0', 
+                    backgroundColor: '#fff',
+                    borderRadius: '12px'
+                    }}>
+                    <Typography variant="h6" sx={{ m: 2 }}>
+                        Monthly Activity Overview
+                    </Typography>
+                    <MonthlyAcitvityChart />
+                </Grid>
+            </Grid>
+
+            {/* Reacent User and Contacts */}
+            <Grid container spacing={2.5} sx={{ mb:4}}>
+                {/* Recent Products  */}
+            <Grid size={ {xs: 12 , md: 7}}>
+
+                </Grid>
+            </Grid>
         </Box>
     </>
 )
 
 export default Dashboard
+
+
+
